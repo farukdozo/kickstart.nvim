84918afbd49c9f5d50cf073002e76a0117b10422
farukdozo <djozo_faruk@hotmail.com>
2025-07-22 16:40:08 +0200

    Added treesitter context & removed navic

--- init.lua
+++ init.lua
@@ -305,33 +305,6 @@ require('lazy').setup({
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
   },
-  -- {
-  --   'SmiteshP/nvim-navic',
-  --   dependencies = { 'neovim/nvim-lspconfig' },
-  --   config = function()
-  --     local navic = require 'nvim-navic'
-  --
-  --     local on_attach = function(client, bufnr)
-  --       if client.server_capabilities.documentSymbolProvider then
-  --         navic.attach(client, bufnr)
-  --       end
-  --     end
-  --
-  --     require('lspconfig').ts_ls.setup {
-  --       on_attach = on_attach,
-  --     }
-  --
-  --     require('lspconfig').lua_ls.setup {
-  --       on_attach = on_attach,
-  --     }
-  --
-  --     navic.setup {
-  --       auto_attach = true,
-  --     }
-  --
-  --     vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
-  --   end,
-  -- },
   {
     'lukas-reineke/indent-blankline.nvim',
     main = 'ibl', -- required for the new v3 API
@@ -339,6 +312,24 @@ require('lazy').setup({
       scope = { enabled = true },
       indent = { char = '▏' },
     }, -- default options; customize if needed
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    opts = {
+      enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+      multiwindow = false, -- Enable multiwindow support.
+      max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+      min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+      line_numbers = true,
+      multiline_threshold = 20, -- Maximum number of lines to show for a single context
+      trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+      mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
+      -- Separator between context and content. Should be a single character string, like '-'.
+      -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+      separator = nil,
+      zindex = 20, -- The Z-index of the context window
+      on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+    },
   },
 
   -- Alternatively, use `config = function() ... end` for full control over the configuration.
